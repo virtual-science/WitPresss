617cbd316a7fe7cd8f67af9f4c7f8c81edb6cdb7
sajid <sajid@gitlab.com>
2016-06-27 12:33:17 +0100

    initial work on V0

---
+++ app/controllers/Accounts.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import javax.persistence.Entity;
+
+import play.*;
+import play.mvc.*;
+import java.util.*;
+import models.*;
+
+public class Accounts extends Controller {
+	public static void login() {
+		render();
+	}
+
+	public static void register(User user, boolean terms) {
+		Logger.info(user.firstName + " " + user.lastName + " " + user.email + " " + user.password);
+		if (terms != false) {
+			user.save();
+			login();
+		} else {
+			signup();
+		}
+	}
+
+	
+	public static void signup() {
+		render();
+	}
+	
+
+	public static void authenticate(String email, String password) {
+		Logger.info("Attempting to authenticate with " + email + ":" + password);
+
+		User user = User.findByEmail(email);
+		if ((user != null) && (user.checkPassword(password) == true)) {
+			Logger.info("Successfully authentication of " + user.firstName);
+			session.put("logged_in_userid", user.id);
+			signup();
+		} else {
+			Logger.info("Authentication failed");
+			login();
+		}
+
+	}
+
+	public static User getCurrentUser() {
+		String userId = session.get("logged_in_userid");
+		if (userId == null) {
+			return null;
+		}
+		User logged_in_user = User.findById(Long.parseLong(userId));
+		Logger.info("In Accounts controller: Logged in user is " + logged_in_user.firstName);
+		return logged_in_user;
+	}
+
+}
